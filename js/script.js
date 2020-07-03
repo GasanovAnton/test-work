38ed5b004cb686f2beebf49255c5d5dfe1c8633b
GasanovAnton <Kicym1992@gmail.com>
2020-07-03 20:45:53 +0300

    Доисправил косяки с позицией, попробовал добавить адаптив
    исправил косяки с поизициями которые нашел, добавил меню в мобильную версию, добавил наведение и нажатие поиску.
    
    исправил косяк с горизонтальной прокруткой в мобильной версии ( не убрал стрелку и не перезадал ширину боди)
    
    немного разобрался в настройках slik slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider').slick({
         dots: true,
-        centerMode: true,   
-        appendDots:$('.slider-nav'),
-        prevArrow:$('.prevArrow'),
-        nextArrow:$('.nextArrow'),
+        centerMode: true,
+        appendDots: $('.slider-nav'),
+        prevArrow: $('.prevArrow'),
+        nextArrow: $('.nextArrow'),
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 815,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    variableWidth: true
+                }
+            }
+        ]
     });
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.main-menu-wrap').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
 })
+
+
+
