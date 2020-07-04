8b52f09d9840cd1547af2baa3858aea925fa2506
GasanovAnton <Kicym1992@gmail.com>
2020-07-05 00:21:35 +0300

    доделал десктопную часть
    добавил блоки:
    counters
    Recent Posts
    partners
    
    и сделал футер

--- js/script.js
+++ js/script.js
@@ -27,6 +27,14 @@ $(document).ready(function () {
         $('.header__burger,.main-menu-wrap').toggleClass('active');
         $('body').toggleClass('lock');
     })
+    $('.recent-posts__slider').slick({
+        dots: false,
+        prevArrow: $('.recent-posts__nav-prevArrow'),
+        nextArrow: $('.recent-posts__nav-nextArrow'),
+        centerMode: true,
+        variableWidth: true,
+        slidesToScroll: 1
+    });
 })
 
 
